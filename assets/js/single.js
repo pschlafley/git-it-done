344c676153122eb41ad914c9174716bc892f299d
Peyton Schlafley <pschlafley0@gmail.com>
2020-09-09 17:50:43 -0500

    when a user clicks on a repository it will direct them to a page that lists all the issues for that repository

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+};
 
 
 function getRepoIssues(repo) {
@@ -18,7 +31,7 @@ function getRepoIssues(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -71,4 +84,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("angular/angular");
+
+getRepoName();
